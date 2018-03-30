fa0eea976c0d0b853c3127983e92d00311539aff
qq524787275 <524787275@qq.com>
2018-03-30 15:29:53 +0800

    第一次提交

---
+++ library/src/main/java/com/zhuzichu/library/utils/Utils.java
@@ -0,0 +1,12 @@
+package com.zhuzichu.library.utils;
+
+/**
+ * 作者: Zzc on 2018-03-30.
+ * 版本: v1.0
+ */
+
+public class Utils {
+    public static final String getVersion(){
+        return "1.0.0";
+    }
+}
